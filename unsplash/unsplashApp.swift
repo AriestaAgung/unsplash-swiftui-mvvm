dd86bf758fd97b1e4f584eaab462179fa9603a24
Ariesta Agung <ariesta.work@gmail.com>
2022-06-01 10:24:52 +0700

    Initial Commit

---
+++ unsplash/unsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  unsplashApp.swift
+//  unsplash
+//
+//  Created by BRIMO on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct unsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
